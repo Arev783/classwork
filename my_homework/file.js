b9952ec94957338024b328d308230fd2aa8e9c8d
Arev <arevg693@gmail.com>
2026-02-23 21:24:01 +0400

    change in dev

--- my_homework/file.js
+++ my_homework/file.js
@@ -1,7 +1,7 @@
 var celsiusValues = [15, 25, 10, 30];
 celsiusValues.forEach(function(item) {
     if(item * 1.8 + 32 >= 70)
-		console.log("warm");
+		console.log("warmmmm");
 	else
 	console.log("cold");
 });
